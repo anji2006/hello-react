fc87fb98e4b0fea42721de066d7bb89b9bda7a8f
anji <aveeranki@nouveau-labs.com>
2023-06-17 16:54:27 +0530

    first step

---
+++ App.js
@@ -0,0 +1,11 @@
+const head = React.createElement("h1", { id: "heading", name: "naeee" }, [
+  "HEllo Anji this is REact",
+  788,
+  "okok",
+]);
+
+console.log("###", head);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(head);
+
+console.log("####roor", root);
